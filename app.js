82d1bf55af57fef5858cdc9de465015ed95ce215
Soumya1002 <soumyasenthil1002@gmail.com>
2024-01-09 20:42:01 +0530

    innerHTML

--- app.js
+++ app.js
@@ -13,7 +13,9 @@ const wrap= document.querySelector('#book-list ul li:nth-child(3) .name');
 
 var books= document.querySelectorAll('#book-list li .name');
 console.log(books);
-Array.from(books).forEach((item)=>{
-    console.log(item);
+books.forEach((item)=>{
+    console.log(item.innerHTML+=" (Book title)");
 })
 
+const booklist =document.querySelector('#book-list ');
+console.log(booklist.innerHTML);
