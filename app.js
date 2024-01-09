42aae886f8fa2b7515cc255bf582ccb14e652464
Soumya1002 <soumyasenthil1002@gmail.com>
2024-01-09 19:39:13 +0530

    queryselector and queryselectorall

--- app.js
+++ app.js
@@ -1,9 +1,19 @@
-var titles=document.getElementsByClassName('title');
+// var titles=document.getElementsByClassName('title');
 
 
-Array.from(titles).forEach(item => {
-    item.style.color='violet';
-   console.log( item.childNodes);
+// Array.from(titles).forEach(item => {
+//     item.style.color='violet';
+//    console.log( item.childNodes);
 
     
-});
+// });
+
+const wrap= document.querySelector('#book-list ul li:nth-child(3) .name');
+//console.log(wrap.firstChild);
+
+var books= document.querySelectorAll('#book-list li .name');
+console.log(books);
+Array.from(books).forEach((item)=>{
+    console.log(item);
+})
+
