8eb06655f0aec9e6e5b7127fcce4d014614a03af
Soumya1002 <soumyasenthil1002@gmail.com>
2024-01-10 08:43:04 +0530

    add books to website

--- app.js
+++ app.js
@@ -17,3 +17,25 @@ list.addEventListener('click', (e)=>{
         list.removeChild(li);
     }
 });
+
+const addForm=document.forms['add-book'];
+addForm.addEventListener('submit', (e)=>{
+    e.preventDefault();
+
+    const val= addForm.querySelector('input[type="text"]').value;
+    console.log(val);
+
+    const element=document.createElement('li');
+    const el=document.createElement('span');  
+    const del=document.createElement('span');
+
+   del.textContent='delete';
+   el.textContent=val;
+
+   el.classList.add("name");
+   del.classList.add('delete');
+
+    element.appendChild(el);
+    element.appendChild(del);
+    list.appendChild(element);
+});
